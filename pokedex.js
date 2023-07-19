56403870ca181999ef9ea2f6963df1936550713b
JuanFelipeCB01 <jfelipecbermudez@gmail.com>
2023-07-20 01:40:40 +0200

    Implementación de sass y JavaScript muestra los pokemons del 1 al 150

--- pokedex.js
+++ pokedex.js
@@ -1,8 +1,7 @@
-const pokedex$$ = document.querySelector("#pokedex");
+const pokedex$$ = document.querySelector(".main-pokedex");
 const arrayPokemons = []
-const mappedPokemons = []
 
-const getPokemons = async ()=>{
+const getAndDrawPokemons = async ()=>{
     for (let i = 1; i <= 150; i++) {   
         const pokemons = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
         const poke = await pokemons.json()
@@ -16,24 +15,36 @@ const getPokemons = async ()=>{
         id: pokemon.id
     }))
 
-    mappedPokemons.push(pokemons)
-
     const drawPokemon = (pokemons)=>{
         for(const pokemon of pokemons){
+            // console.log(pokemon)
             let pokeDiv$$ = document.createElement("div")
+            pokeDiv$$.setAttribute("class", `main-pokedex-card ${pokemon.type}`)
             pokeDiv$$.innerHTML =
             `
-                <h2>${pokemon.name}</h2>
-                <img src=${pokemon.image} alt="${pokemon.name}"/>
-                <p>${pokemon.type}</p>
+            <div class="main-pokedex-card-inner">
+                <div class="main-pokedex-card-inner-front">
+                    <img src="images/pokemon_card_backside.png" alt="Avatar" style="width:300px">
+                </div>
+                <div class="main-pokedex-card-inner-back">
+                    <h2 class='card-title'>${pokemon.name}</h2>
+                    <img src=${pokemon.image} alt="${pokemon.name}" class='card-image'/>
+                    <p class='card-subtitle'>Type: ${pokemon.type}</p>
+                </div>
+            </div>
             `
             pokedex$$.appendChild(pokeDiv$$)
         }
     }
-    console.log(mappedPokemons)
-    drawPokemon(mappedPokemons)
+    drawPokemon(pokemons)
 }
 
-getPokemons()
+// const filterByType = (div$$)=>{
+//     if(div$$)
+
+// }
+
+
+getAndDrawPokemons()
 
 
